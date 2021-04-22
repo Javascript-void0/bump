418fb50b0dce127ba2fa96ddcbffc0ca79db7a07
Java <indead1124@gmail.com>
2021-04-21 22:51:08 -0400

    cleanup

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import asyncio
 from discord.ext import commands
 from discord.utils import get
 
-client = commands.Bot(command_prefix='??')
+client = commands.Bot(command_prefix='.')
 TOKEN = os.getenv("TOKEN")
 
 @client.event
@@ -14,15 +14,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    embed = discord.Embed(title = "Disboard is off cooldown!", description  = "Time to bump! 🍌", color = discord.Color.dark_blue())
-    embed.set_thumbnail(url="https://i.pinimg.com/originals/ee/b0/e6/eeb0e632af64b76830c5777e07770202.png")
-    guild = client.get_guild(802565984602423367)
-    channel = guild.get_channel(802577298267963412)
-    if message.author.id == 302050872383242240 and 'disboard.org' in message.embeds[0].description:
+    if message.author.id == 302050872383242240 and 'Check it on DISBOARD:' in message.embeds[0].description:
+        embed = discord.Embed(title = "Disboard is off cooldown!", description  = "Time to bump! 🍌", color = discord.Color.dark_blue())
+        embed.set_thumbnail(url="https://i.pinimg.com/originals/ee/b0/e6/eeb0e632af64b76830c5777e07770202.png")
+        guild = client.get_guild(802565984602423367)
+        channel = guild.get_channel(802577298267963412)
         print('Bump Detected :D')
         cd = 7201
         print('Countdown Started')
-        while cd >= 0:
+        while cd > 0:
             cd -= 1
             await asyncio.sleep(1)
             print(cd)
