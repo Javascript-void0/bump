4ab4a6eec06ca51cf3ddb6a6e7ebdc122e68a605
Java <indead1124@gmail.com>
2021-04-21 22:57:45 -0400

    Manual Start

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord.utils import get
 
 client = commands.Bot(command_prefix='.')
+client.remove_command('help')
 TOKEN = os.getenv("TOKEN")
 
 @client.event
@@ -22,6 +23,20 @@ async def on_message(message):
         print('Bump Detected :D')
         cd = 7201
         print('Countdown Started')
+        while cd > 0:
+            cd -= 1
+            await asyncio.sleep(1)
+            print(cd)
+        await channel.send(embed=embed)
+        print('Reminder Sent')
+
+    elif message.author.id == 594352318464524289 and '.bump_start' in message.content:
+        embed = discord.Embed(title = "Disboard is off cooldown!", description  = "Time to bump! 🍌", color = discord.Color.dark_blue())
+        embed.set_thumbnail(url="https://i.pinimg.com/originals/ee/b0/e6/eeb0e632af64b76830c5777e07770202.png")
+        guild = client.get_guild(802565984602423367)
+        channel = guild.get_channel(802577298267963412)
+        print('Reminder Manually Started')
+        cd = 7201
         while cd > 0:
             cd -= 1
             await asyncio.sleep(1)
